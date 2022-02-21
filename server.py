fe575bb043ea5a67926db8d0df70f3c5bae9e22b
Ilya Varnavskiy <ilya-varnavskiy@users.noreply.github.com>
2022-02-21 17:54:26 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,27 @@
 import os
-import http.server
+#import http.server
 import socketserver
 
 from http import HTTPStatus
 
+import cherrypy
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
-        self.wfile.write(msg.encode())
+#class Handler(http.server.SimpleHTTPRequestHandler):
+#    def do_GET(self):
+#        self.send_response(HTTPStatus.OK)
+#        self.end_headers()
+#        msg = 'Hello! you requested %s' % (self.path)
+#        self.wfile.write(msg.encode())
 
 
 port = int(os.getenv('PORT', 80))
 print('Listening on port %s' % (port))
-httpd = socketserver.TCPServer(('', port), Handler)
-httpd.serve_forever()
+#httpd = socketserver.TCPServer(('', port), Handler)
+#httpd.serve_forever()
+
+class demoExample:
+   def index(self):
+      return "Hello World!!!"
+   index.exposed = True
+
+cherrypy.quickstart(demoExample())
